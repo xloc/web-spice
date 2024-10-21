97cfdb322e8f802f299d5b224f7dd6bb1607b920
xloc <xloc.cc@outlook.com>
2024-10-20 23:23:27 -0700

    preliminary schematic rendering

--- src/components/SchematicView.vue
+++ src/components/SchematicView.vue
@@ -1,25 +1,105 @@
 <template>
-  <div class="">
-    <ul>
-      <li v-for="device in circuit.devices">
-        {{ device.id }}
-      </li>
-    </ul>
-    <ul>
-      <li v-for="net in circuit.nets">
-        {{ net.id }}
-      </li>
-    </ul>
+  <div ref="container" class="relative">
+
+    <svg ref="plot-svg"></svg>
+
+    <button class="absolute right-0 bottom-0 p-2
+     bg-slate-500 hover:bg-slate-400 text-white rounded-lg">
+      <RectangleGroupIcon class="size-6 " />
+    </button>
+
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import { Circuit } from '../model/Circuit';
+import { ref, useTemplateRef, watchEffect } from 'vue';
+import { Circuit, Device } from '../model/Circuit';
+import * as d3 from 'd3';
+import { useElementSize } from '@vueuse/core';
+import { RectangleGroupIcon } from '@heroicons/vue/24/solid'
 
 const props = defineProps<{
   circuit: Circuit
 }>();
+const svgElement = useTemplateRef('plot-svg')
+
+const container = useTemplateRef('container')
+const { width, height } = useElementSize(container)
+
+
+
+watchEffect(() => {
+  const { circuit } = props;
+  if (!circuit) return;
+
+  const svg = d3.select(svgElement.value)
+    .attr('width', width.value)
+    .attr('height', height.value);
+
+  const nodes = circuit.devices;
+
+
+  const drag = d3.drag<SVGCircleElement, Device>()
+    .on("start", function () { d3.select(this).attr("stroke", "black"); })
+    .on("drag", function (event, d) {
+      d3.select(this).raise()
+        .attr("cx", d.x = event.x)
+        .attr("cy", d.y = event.y);
+      simulation.alpha(0.2).restart();
+      update()
+    })
+    .on("end", function () { d3.select(this).attr("stroke", null); });
+
+
+  const d3node = svg.selectAll("circle")
+    .data(nodes)
+    .join("circle")
+    .attr("cx", d => d.x)
+    .attr("cy", d => d.y)
+    .attr("r", 20)
+    .attr("fill", d => Circuit.colorMap[d.type.toUpperCase()] ?? 'black')
+    .call(drag as any)
+
+  const update = () => {
+    svg.selectAll('text')
+      .data(nodes)
+      .join('text')
+      .style('user-select', 'none')
+      .attr('x', (d: any) => d.x)
+      .attr('y', (d: any) => d.y)
+      .attr('dx', 30)
+      .attr('dy', 7)
+      .text((d: any) => d.id)
+
+    const links = svg.selectAll("line")
+      .data(circuit.getLinks())
+      .join("line")
+      .attr("x1", d => nodes[d[0]].x)
+      .attr("y1", d => nodes[d[0]].y)
+      .attr("x2", d => nodes[d[1]].x)
+      .attr("y2", d => nodes[d[1]].y)
+      .attr("stroke", "black")
+      .attr("stroke-width", 2);
+  }
+
+
+
+  const simulation = d3.forceSimulation(nodes)
+    .force("charge", d3.forceManyBody().strength(-5))
+    .force("center", d3.forceCenter(width.value / 2, height.value / 2))
+    .force("link", d3
+      .forceLink(circuit.getLinks().map(l => ({ source: l[0], target: l[1] })))
+      .id((d, i) => { return i }).distance(200).strength(0.1)
+    )
+
+  simulation.on('tick', () => {
+    d3node
+      .attr('cx', (d: any) => d.x)
+      .attr('cy', (d: any) => d.y);
+    update();
+  });
+
+})
 
 
 </script>
--- src/model/Circuit.ts
+++ src/model/Circuit.ts
@@ -1,12 +1,13 @@
 import type Parser from 'web-tree-sitter';
 import _ from 'lodash';
+import * as d3 from 'd3';
 
 export interface Device {
   id: string;
   type: string;
 
-  // x: number;
-  // y: number;
+  x: number;
+  y: number;
   // rotation: number;
 
   nodes: string[];
@@ -27,32 +28,56 @@ export class Circuit {
 
   static fromTree(tree: Parser.Tree): Circuit {
     const circuit = new Circuit();
-
     const instanceLines = tree.rootNode.descendantsOfType('instance_line');
+
     circuit.devices = instanceLines.map(instanceLine => {
       const type = instanceLine.childForFieldName('device_type')!.text;
       const name = instanceLine.childForFieldName('device_name')!.text;
       const nodes = instanceLine.descendantsOfType('node').map(node => node.text)
-
       if (!type || !name) throw new Error('Invalid device');
-
 
       const device: Device = {
         id: type + name,
         type: type,
-        nodes: nodes
-        // type: instanceLine.childForFieldName('device')!.text,
-        // x: Number(instanceLine.childForFieldName('x')!.text),
-        // y: Number(instanceLine.childForFieldName('y')!.text),
+        nodes: nodes,
+        x: 0,
+        y: 0,
         // rotation: Number(instanceLine.childForFieldName('rotation')!.text),
       };
-
       return device;
     });
-
     circuit.nets = _(circuit.devices).flatMap(device => device.nodes).uniq().map(node => ({ id: node })).value();
 
     return circuit;
+  }
+
+  getLinks(): [number, number][] {
+    const nodeToDeviceIndex = new Map<string, number[]>();
+    this.devices.forEach((device, index) => {
+      device.nodes.forEach(node => {
+        if (!nodeToDeviceIndex.has(node))
+          nodeToDeviceIndex.set(node, []);
+        nodeToDeviceIndex.get(node)!.push(index);
+      });
+    });
+
+    const links: [number, number][] = [];
+    this.nets.forEach(net => {
+      const deviceIndices = nodeToDeviceIndex.get(net.id);
+      if (!deviceIndices || deviceIndices.length < 2) return;
+      for (let i = 0; i < deviceIndices.length; i++)
+        for (let j = i + 1; j < deviceIndices.length; j++)
+          links.push([deviceIndices[i], deviceIndices[j]]);
+    });
+    return links;
+  }
+
+  static colorMap: Record<string, string> = {
+    'R': d3.schemeTableau10[0],
+    C: d3.schemeTableau10[1],
+    'L': d3.schemeTableau10[2],
+    V: d3.schemeTableau10[3],
+    Q: d3.schemeTableau10[4],
   }
 
 }
